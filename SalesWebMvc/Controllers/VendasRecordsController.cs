43afd6bac6fb37e401471e635be8b4434ea288a5
Matheus Alves <matheus.180960@gmail.com>
2022-06-20 17:06:21 -0300

    Criando as nevegações das paginas

---
+++ SalesWebMvc/Controllers/VendasRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class VendasRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+        public IActionResult AgrupamentoSimples()
+        {
+            return View();
+        }
+    }
+}
